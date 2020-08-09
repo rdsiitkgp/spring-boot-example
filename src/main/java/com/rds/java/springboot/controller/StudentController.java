91fae210a856f4c3297360e8c2d6e5deef58a162
rdsiitkgp <rds.iitkgp@gmail.com>
2020-08-08 20:52:24 -0700

    Adding query param

--- src/main/java/com/rds/java/springboot/controller/StudentController.java
+++ src/main/java/com/rds/java/springboot/controller/StudentController.java
@@ -5,13 +5,15 @@ import java.util.List;
 
 import com.rds.java.springboot.model.Student;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
 	
-	@RequestMapping("university/v1//students")
-    public List<Student> getStudents(){
-		return Arrays.asList(new Student(123, "Binod", "binod@yahoo.com"));
+	@RequestMapping("university/v1/students")
+    public List<Student> getStudents(@RequestParam String email) throws InterruptedException {
+	    Thread.sleep(100);
+		return Arrays.asList(new Student(123, "Binod", email + "@yahoo.com"));
     }
 }
